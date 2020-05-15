c408d403c006b56a4438b5e05c3cdc44a846f951
william9969 <wsinchi69@gmail.com>
2020-05-14 22:29:10 -0500

    Validaciones de los campos completamente terminadas

--- Validaciones/validaciones.js
+++ Validaciones/validaciones.js
@@ -4,6 +4,7 @@ var banderaCor = false;
 var banderaNom = false;
 var banderaApe = false;
 var banderaCon =false;
+var banderaFec = false;
 
 function validarCedula(elemento)
 {   
@@ -55,6 +56,7 @@ function validarCedula(elemento)
                 else
                 {
                     document.getElementById('mensajeCedula').innerHTML = '<br>La cedula no es valida';
+                    document.getElementById('mensajeCedula').style.color ='red';
                     document.getElementById('cedula').style.border = '2px red solid';
                 }
                 return true
@@ -84,7 +86,7 @@ function validarTelefono(telefono){
         if(miAscii >= 48 && miAscii <= 57){
             if(telefono.value.length == 10){
             document.getElementById('mensajeTelefono').innerHTML = '<br>Telefono valido';
-            telefono.style.border = '2px green solid';
+            telefono.style.border = '2px chartreuse solid';
             document.getElementById('mensajeTelefono').style.color ='white';
             banderaTel=true;
             }
@@ -119,7 +121,7 @@ function validarNombres(nombres){
                         console.log(arrayNombres.length+"X222");
                         document.getElementById('mensajeNombres').innerHTML = '<br>Nombre Valido';
                         document.getElementById('mensajeNombres').style.color ='white';
-                        nombres.style.border = '2px green solid';
+                        nombres.style.border = '2px chartreuse solid';
                         return true;
                     }
                     else {
@@ -154,7 +156,7 @@ function validarApellidos(apellidos){
                         console.log(arrayApellidos.length+"X222");
                         document.getElementById('mensajeApellidos').innerHTML = '<br>Apellido Valido';
                         document.getElementById('mensajeApellidos').style.color ='white';
-                        apellidos.style.border = '2px green solid';
+                        apellidos.style.border = '2px chartreuse solid';
                         return true;
                     }
                     else {
@@ -183,7 +185,7 @@ function validarCorreo(correo){
         if (/^\w+([\.-]?\w+)*@(?:|est)\.(?:|ups)\.(?:|edu)\.(?:|ec)+$/.test(correo.value) || /^\w+([\.-]?\w+)*@(?:|ups)\.(?:|edu)\.(?:|ec)+$/.test(correo.value) ){
                 document.getElementById('mensajeCorreo').innerHTML = '<br>El Correo valido';
                 document.getElementById('mensajeCorreo').style.color ='white';
-                correo.style.border = '2px green solid';
+                correo.style.border = '2px chartreuse solid';
                 banderaCor=true;
                 return true;
         } else {
@@ -201,7 +203,7 @@ function validarCorreo(correo){
     }
 }
 
-function   validarContraseña( contrasena){
+function validarContraseña(contrasena){
     //arrayApellidos=contraseña.value;
     //var miAscii = contrasena.value.charCodeAt(contrasena.value.length-1);
     if(contrasena.value.length >= 8)
@@ -245,8 +247,8 @@ function   validarContraseña( contrasena){
             banderaCon=true;
             console.log("Contaseña")
             document.getElementById('mensajeContrasena').innerHTML = '<br>Contraseña valida';
-            document.getElementById('mensajeContrasena').style.color='white';
-            contrasena.style.border = '2px green solid';
+            document.getElementById('mensajeContrasena').style.color='chartreuse';
+            contrasena.style.border = '2px chartreuse solid';
             return true;
         }
         else {
@@ -261,52 +263,52 @@ function   validarContraseña( contrasena){
         return false;
     }
 }
+function validarFecha(fecha){
+    var miAscii = fecha.value.charCodeAt(fecha.value.length-1);
+    if(fecha.value.length <11){
+            if((miAscii >= 48 && miAscii <= 57) || miAscii ==47){
+                arrayFecha = fecha.value.split("/");
+                var dia=arrayFecha[0];
+                var mes = arrayFecha[1];
+                var anio = arrayFecha[2];
+                if((dia>0 && dia<32) && (mes>0 && mes<13) && (anio>1900 && anio<2020))
+                {
+                    banderaFec=true;
+                    console.log("Contaseña")
+                    document.getElementById('mensajeFecha').innerHTML = '<br>Fecha valida';
+                    document.getElementById('mensajeFecha').style.color='white';
+                    fecha.style.border = '2px chartreuse solid';
+                    return true;
+                }
+                else {
+                    banderaFec=false;
+                    
+                    document.getElementById('mensajeFecha').innerHTML = '<br>Fecha no valida';
+                    document.getElementById('mensajeFecha').style.color='red';
+                    fecha.style.border = '2px red solid';
+                    return false;
+                }
+                //console.log(miAscii)
+            }
+            else {
+                fecha.value = fecha.value.substring(0, fecha.value.length-1);
+                document.getElementById('mensajeFecha').innerHTML = '<br>Fecha no valida';
+                document.getElementById('mensajeFecha').style.color='red';
+                fecha.style.border = '2px red solid';
+                return false
+            }
+    }else{
+        fecha.value = fecha.value.substring(0, fecha.value.length-1);
+    }
+}
 
 function botonSubmit(){
 
-    if(banderaCed==true && banderaTel==true&&banderaCor==true){
+    if(banderaCed==true && banderaTel==true && banderaCor==true && banderaNom==true && banderaApe==true && banderaCon==true && banderaFec==true){
+        
         alert('Datos Correctos')
     }
     else{
         alert('Datos Incorrectos')
     }
 }
-/*
-function validarCamposObligatorios()
-{
-    var bandera = true
-
-    for(var i = 0; i < document.forms[0].elements.length; i++){
-    var elemento = document.forms[0].elements[i]
-    if(elemento.value == '' && elemento.type == 'text'){
-    if(elemento.id == 'cedula'){
-    document.getElementById('mensajeCedula').innerHTML = '<br>La cedula esta vacia'
-    }
-    elemento.style.border = '1px red solid'
-    elemento.className = 'error'
-    bandera = false
- }
- }
-
- if(!bandera){
-    alert('Error: revisar los comentarios')
- }
- return bandera
-}
-function validarLetras(elemento)
-{
-    if(elemento.value.length > 0){
-        var miAscii = elemento.value.charCodeAt(elemento.value.length-1)
-        console.log(miAscii)
-
-    if(miAscii >= 97 && miAscii <= 122){
-        return true
-    }else {
-            elemento.value = elemento.value.substring(0, elemento.value.length-1)
-            return false
-        }
-    }else{
-    return true
-}
-
-}*/
